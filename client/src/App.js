34b694a803d2eb978b4d1f8e123cab0d48677a81
ChristoherHenao <cq.henao@gmail.com>
2021-06-16 03:17:23 -0400

    mvp done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Switch, Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+
 
 import SavedList from './Movies/SavedList';
 
@@ -14,6 +18,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -26,11 +31,20 @@ export default function App () {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+  console.log(movieList)
+
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path='/'>
+          <MovieList movies={movieList}/>
+        </Route>
+        <Route path='/movies/:id'>
+          <Movie/>
+        </Route>
+      </Switch>
     </div>
   );
 }
